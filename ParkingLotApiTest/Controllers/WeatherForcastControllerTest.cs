07756492cc8a50d7ba87091377fa7f9e16fa1c58
Wang Jing <jwang315@slb.com>
2023-11-08 13:05:20 +0800

    test: add test for weather forecast.

---
+++ ParkingLotApiTest/Controllers/WeatherForcastControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotApiTest.Controllers
+{
+    public class WeatherForcastControllerTest
+    {
+        private HttpClient _httpClient;
+        public WeatherForcastControllerTest()
+        { 
+            WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>();
+            this._httpClient = webApplicationFactory.CreateClient();
+        }
+        [Fact]
+        public async Task Should_return_correctly_when_get_weather_forecast()
+        {
+            // Given & When
+            HttpResponseMessage response = await _httpClient.GetAsync("/WeatherForecast");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
