61a3e9dffaf9f719ad3bb4d9174586a902c87955
Shuhan Jin <sjin9@slb.com>
2023-11-08 13:22:18 +0800

    refractor: extract testbase for weatherforecast api test

--- ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
+++ ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
@@ -8,20 +8,21 @@ using System.Threading.Tasks;
 
 namespace ParkingLotApiTest.Controllers
 {
-    public class WeatherForecastControllerTest
+    public class WeatherForecastControllerTest : TestBase
     {
         private HttpClient httpClient;
 
-        public WeatherForecastControllerTest()
+        public WeatherForecastControllerTest(WebApplicationFactory<Program> factory) : base(factory)
         {
-            WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>(); //inject Program
-            httpClient = webApplicationFactory.CreateClient();
+            //WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>(); //inject Program
+            //httpClient = webApplicationFactory.CreateClient();
         }
 
         [Fact]
         public async Task Should_return_correctly_when_get_weather_forecast()
         {
             //given&when
+            httpClient = GetClient();
             HttpResponseMessage response = await httpClient.GetAsync("/WeatherForecast");
 
             //then
