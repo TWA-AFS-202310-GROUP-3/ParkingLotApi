012e301de4ee92afab13bb1705fcdedb553c9e9b
YChen110 <Ychen110@slb.com>
2023-11-08 17:07:30 +0800

    refactor: add data converter between dto and entity

--- ParkingLotApi/Dtos/ParkingLotDto.cs
+++ ParkingLotApi/Dtos/ParkingLotDto.cs
@@ -1,7 +1,11 @@
-﻿namespace ParkingLotApi.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingLotApi.Dtos
 {
     public class ParkingLotDto
     {
+        [Required]
+        public string Id { get; set; }
         public string Name { get; set; }    
         public int Capacity { get; set; } 
         public string Location { get; set; }
--- ParkingLotApi/Utils/DataConverter.cs
+++ ParkingLotApi/Utils/DataConverter.cs
@@ -5,13 +5,35 @@ namespace ParkingLotApi.Utils
 {
     public class DataConverter
     {
+        public static ParkingLotEntity ConvertRequestToParkingLotEntity(ParkingLotRequestDto parkingLotRequestDto)
+        {
+            return new ParkingLotEntity()
+            {
+                Name = parkingLotRequestDto.Name,
+                Capacity = parkingLotRequestDto.Capacity,
+                Location = parkingLotRequestDto.Location
+            };
+        }
+
         public static ParkingLotEntity ConvertToParkingLotEntity(ParkingLotDto parkingLotDto)
         {
             return new ParkingLotEntity()
             {
+                Id = parkingLotDto.Id,
                 Name = parkingLotDto.Name,
                 Capacity = parkingLotDto.Capacity,
                 Location = parkingLotDto.Location
+            };
+        }
+
+        public static ParkingLotDto ConvertToParkingLotDto(ParkingLotEntity parkingLotEntity)
+        {
+            return new ParkingLotDto()
+            {
+                Id = parkingLotEntity.Id,
+                Name = parkingLotEntity.Name,
+                Capacity = parkingLotEntity.Capacity,
+                Location = parkingLotEntity.Location
             };
         }
     }
