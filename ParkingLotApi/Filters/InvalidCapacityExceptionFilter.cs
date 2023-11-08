3de9eeacc71bb450bff8d5a9e547bb23197edf1b
Pengyu <PChen10@slb.com>
2023-11-08 15:28:59 +0800

    refactor: use filter to handle invalid capacity exception

---
+++ ParkingLotApi/Filters/InvalidCapacityExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ParkingLotApi.Exceptions;
+
+namespace ParkingLotApi.Filters
+{
+    public class InvalidCapacityExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        int IOrderedFilter.Order => int.MaxValue - 10;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        void IActionFilter.OnActionExecuted(ActionExecutedContext context)
+        {
+            if(context.Exception is InvalidCapacityException invalidCapacityException)
+            {
+                context.Result = new BadRequestResult();
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
